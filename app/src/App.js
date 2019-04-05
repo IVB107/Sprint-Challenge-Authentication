036c6017c4e6ce5626b66d3408f13d9596fc9dce
IVB107 <ianvb107@gmail.com>
2019-04-05 12:16:56 -0500

    Setup CRA, Configured App, Index.js, Register

---
+++ app/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Route, NavLink, withRouter } from 'react-router-dom';
+
+import './App.css';
+import Register from './components/register/Register';
+import Login from './components/login/Login';
+import Jokes from './components/jokes/Jokes';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <NavLink to="/">Home</NavLink>
+          &nbsp;|&nbsp;
+          <NavLink to="/login">Login</NavLink>
+          &nbsp;|&nbsp;
+          <NavLink to="/jokes">Jokes</NavLink>
+          &nbsp;|&nbsp;
+          <NavLink to="/register">Register</NavLink>
+          <button onClick={this.handleLogout}>Logout</button>
+        </header>
+        <main>
+          <Route path="/" exact component={Home} />
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          <Route path="/jokes" component={Jokes} />
+        </main>
+      </div>
+    );
+  }
+
+}
+
+const Home = () => {
+  return <h1>CAUTION: Dad Jokes Incoming...</h1>
+}
+
+export default withRouter(App);
---
+++ app/src/components/register/Register.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import axios from 'axios';
+
+class Register extends Component {
+  state = {
+    username: '',
+    password: ''
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmitForm}>
+          <div>
+            <label htmlFor="username"></label>
+            <input 
+              value={this.state.username}
+              onChange={this.handleInputChange}
+              name="username"
+              type="text"
+              placeholder="username"
+            />
+          </div>
+          <div>
+            <label htmlFor="password"></label>
+            <input 
+              value={this.state.password}
+              onChange={this.handleInputChange}
+              name="password"
+              type="text"
+              placeholder="password"
+            />
+          </div>
+          <div>
+            <button type="submit"></button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+
+  handleInputChange = e => {
+    const { id, value } = e.target;
+    this.setState({
+      [id]: value
+    })
+  }
+
+  handleSubmitForm = e => {
+    e.preventDefault();
+    const endpoint = 'http://localhost:3300/api/register'
+
+    axios
+      .post(endpoint, this.state)
+      .then(res => {
+        console.log(res);
+        localStorage.setItem('token, res.data.token');
+        this.props.history.push('/users');
+      })
+      .catch(err => {
+        console.log('Registration Error: ', err);
+      })
+  }
+
+}
+
+export default withRouter(Register);
